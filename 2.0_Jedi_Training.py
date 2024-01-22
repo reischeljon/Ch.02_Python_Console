231a4bedf0299a8a6db9e428b1f167f678686412
reischeljon <reischeljon@urbandaleschools.com>
2024-01-22 09:04:20 -0600

    10 lines

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,25 +1,26 @@
 '''
-2.0 Jedi Training (20pts)  Name:________________
+2.0 Jedi Training (20pts)  Name:Jonathan Reiscjel
 
 UPLOAD YOUR CH 2 PDF LOG! (6pts)
 
 1.) How do you enter a single line comment in a program? Give an example. (1pt)
-
+# By using the pound/hashtag symbol your able to wtie out a single line comment. An example would be
+# This is Marc Hermon's Chapter 2 log
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
 Then in the Python Console window, enter a=2 and b=5 and then all of the following and record the actual output.
 If the output is an error record the error and try to determine what the error is!
 Comment about any of your predictions that didn't match the actual output
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 =2.5
+b//a                =.1 # I thought that it was dividing 2 by 5 twice it was actually floor division with an answer of 2
+b**a                =25
+b%a                 =0 # I did not know what this equation meant so I put in a random number reading over the lesson i figured out that this was the modulus leaving a remainder of 1
+a+B                 =2 # this was an error as B deos not equal b. B deos not have a set value so the equation cant solve leaving an error message
+type(42)            =42 # This one type an integer or int as 42 is a whole number
+type(42.0)          =42.0 # The program typed float which is not a whole number as 42.0 is a decimal
+type("C3PO")        =C3P0 # This one confused me as it typed str or string as it was multiple characters in 1 line of code
+type(True)          =True # This typed Boolean as it represents True or False
 
 
 3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
@@ -31,22 +32,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = # I predicted it would type out int as I think the answer is a positive whole number
+type(a)       = # it typed out float which had surprised me, but I realized that because the answer was 15.0 it was a deciamal
 
 
 4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
 
-x,y = (4,5)
-a = 3(x + y)
-a
+x,y = (4,5) # it is x,y= (4,5)
+a = 3(x + y) # 3 has not yet been defined so the correct answer would be a = 3*(x + y) by adding * this tells the code not to call 3 but to multiply to the answer
+a = 27
 
 
 5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+ave = x+y+z/3 # the error in this code was the space between the equal sign and the equation the right answer looks like ave =x=y+z/3
+ave = 8.6 repeating
 
 
 '''
